754049ed491c27cf7d8f7420f5a4f26727c1f7fb
svetlana <agaevi@bk.ru>
2024-11-18 19:39:02 +0300

    done

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    public Deth8() {
+    }
 }
